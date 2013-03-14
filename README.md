2aeac5f29439486d9f937622520036085e8ac305
bluelyte <bluelyte382@gmail.com>
2013-03-14 08:01:00 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# bluelyte/transmission-remote
+
+A PHP wrapper for the transmission-remote CLI utility.
+
+DISCLAIMER: This project is not endorsed by, affiliated with, or intended to infringe upon the [Transmission project](http://www.transmissionbt.com) and is meant for non-commercial purposes (i.e. personal use) only.
+
+# Install
+
+The recommended method of installation is [through composer](http://getcomposer.org/).
+
+```JSON
+{
+    "require": {
+        "bluelyte/transmission-remote": "1.0.0"
+    }
+}
+```
+
+# Usage
+
+```php
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+// Configuration and initialization
+$remote = new \Bluelyte\Transmission\Remote();
+$remote->setEncryption(false); // not recommended - is true by default
+$remote->setPort(12345); // uses standard BitTorrent port by default
+$remote->setUpnp(true); // is false by default
+$remote->setDownloadPath('/path/for/downloads');
+$remote->start();
+
+// Handling torrents
+$remote->addTorrents('/path/to/file1.torrent');
+$remote->addTorrents('/path/to/file2.torrent /path/to/file3.torrent');
+$remote->addTorrents(array('/path/to/file4.torrent', '/path/to/file5.torrent'));
+$remote->startTorrents();
+
+// Debugging
+var_dump($remote->getOutput(), $remote->getStatus());
+*/
+```
+
+## License
+
+Released under the BSD License. See `LICENSE`.
---
+++ src/Remote.php
@@ -0,0 +1,174 @@
+<?php
+
+namespace Bluelyte\Transmission;
+
+/**
+ * A wrapper for the transmission-remote CLI utility.
+ *
+ * @link http://blog.zloether.com/2009/03/command-line-bittorrent-with.html
+ */
+class Remote
+{
+    /**
+     * Flag for whether peers using encryption should be required or only 
+     * preferred
+     * @var boolean TRUE to require encryption, FALSE otherwise
+     */
+    protected $encryption = true;
+
+    /**
+     * Port on which to operate the daemon
+     * @var string
+     */
+    protected $port = '58261';
+
+    /**
+     * Flag for whether to enable UPnP port mapping
+     * @var boolean TRUE to enable port mapping, FALSE otherwise
+     */
+    protected $upnp = false;
+
+    /**
+     * Path at which to store downloaded files
+     * @var string
+     */
+    protected $downloadPath = null;
+
+    /**
+     * Output of the last executed daemon command
+     * @var array
+     */
+    protected $output = array();
+
+    /**
+     * Exit status of the last executed daemon command
+     * @var int
+     */
+    protected $status = 0;
+
+    /**
+     * Flag indicating whether the daemon has been started
+     * @var boolean
+     */
+    protected $started = false;
+
+    /**
+     * Sets a flag for whether peers using encryption should be required or only 
+     * preferred.
+     *
+     * @param boolean $encryption TRUE to require encryption, FALSE otherwise
+     */
+    public function setEncryption($encryption)
+    {
+        $this->encryption = (boolean) $encryption;
+    }
+
+    /**
+     * Sets the port on which to operate the daemon.
+     *
+     * @param string $port
+     */
+    public function setPort($port)
+    {
+        if (!ctype_digit($port)) {
+            trigger_error('Port must be a positive integer: ' . var_export($port, true), E_USER_ERROR);
+        }
+        $this->port = $port;
+    }
+
+    /**
+     * Sets a flag for whether to enable UPnP port mapping.
+     *
+     * @param boolean TRUE to enable port mapping, FALSE otherwise
+     */
+    public function setUpnp($upnp)
+    {
+        $this->upnp = (boolean) $upnp;
+    }
+
+    /**
+     * Sets the path at which to store downloaded files.
+     *
+     * @param string $downloadPath
+     */
+    public function setDownloadPath($downloadPath)
+    {
+        if (!is_dir($downloadPath) || !is_writable($downloadPath)) {
+            trigger_error('Cannot write to directory: ' . $downloadPath, E_USER_ERROR);
+        }
+        $this->downloadPath = $downloadPath;
+    }
+
+    /**
+     * Execute a shell command.
+     *
+     * @param string $command
+     */
+    protected function execute($command)
+    {
+        exec($command, $this->output, $this->status);
+    }
+
+    /**
+     * Returns the output of the last daemon command.
+     *
+     * @return array
+     */
+    public function getOutput()
+    {
+        return $this->output;
+    }
+
+    /**
+     * Returns the exit status of the last daemon command.
+     *
+     * @return int
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * Starts the daemon.
+     */
+    public function start()
+    {
+        if ($this->started) {
+            return;
+        }
+
+        $this->execute('transmission-daemon');
+        
+        $command = 'transmission-remote -C'
+            . ' -' . ($this->encryption ? 'er' : 'ep')
+            . ' -p ' . $this->port
+            . ' -' . ($this->upnp ? 'm' : 'M')
+            . ($this->downloadPath ? ' -w ' . $this->downloadPath : '');
+        $this->execute($command);
+
+        $this->started = true;
+    }
+
+    /**
+     * Adds a torrent to download.
+     *
+     * @param string|array $path Space-delimited list or array of paths to 
+     *        one or more .torrent files
+     */
+    public function addTorrents($paths)
+    {
+        if (is_array($paths)) {
+            $paths = implode(' ', $paths);
+        }
+        $this->execute('transmission-remote -a ' . $paths);
+    }
+
+    /**
+     * Starts downloading all added torrents.
+     */
+    public function startTorrents()
+    {
+        $this->execute('transmission-remote -tall --start');
+    }
+}
